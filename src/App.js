8b3a95c125bcc56bb1af44e240ef60c7f8eb567d
Martín Valetta <martin.valetta.50@gmail.com>
2024-03-15 19:45:17 -0300

    logica del carrito

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar />
-        <div className="main-content" style={{ marginTop: '5rem' }}>
-          <Routes>
-            <Route path='/' element={<ItemListContainer />} />
-            <Route path='/category/:categoryId' element={<ItemListContainer />} />
-            <Route path='/item/:productId' element={<ItemDetailContainer />} />
-            <Route path='*' element={<h1>404</h1>} />
-          </Routes>
-        </div>
+        <CartProvider>
+          <NavBar />
+          <div className="main-content" style={{ marginTop: '5rem' }}>
+            <Routes>
+              <Route path='/' element={<ItemListContainer />} />
+              <Route path='/category/:categoryId' element={<ItemListContainer />} />
+              <Route path='/item/:productId' element={<ItemDetailContainer />} />
+              <Route path='*' element={<h1>404</h1>} />
+            </Routes>
+          </div>
+        </CartProvider>
       </BrowserRouter>
     </div> 
   );
